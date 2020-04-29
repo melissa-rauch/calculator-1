2d8b9af7a01b52c95fc33ce91776589df85478d7
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-04-29 19:38:31 +0000

    Completed power function

--- arithmetic.py
+++ arithmetic.py
@@ -34,7 +34,8 @@ def cube(num1):
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    power_of = num1 ** num2
+    return power_of
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
